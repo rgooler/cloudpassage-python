186a7bf5168d7a1f2acee19129f165336f39f4c7
Ryan Gooler <rgooler@desk.com>
2014-01-03 01:00:35 +0000

    Nearly able to create server groups, but I am getting errors that are quite baffling

--- cloudpassage/cloudpassage.py
+++ cloudpassage/cloudpassage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import requests
+from json import dumps
 from base64 import urlsafe_b64encode as b64encode
 from datetime import datetime, timedelta
 
@@ -32,17 +33,21 @@ class InvalidClientError(Exception):
 class Token(str):
     """Handle automagically refreshing expired tokens"""
 
-
-    baseurl = "https://api.cloudpassage.com/" #URL to access the cloudpassage API
+    # URL to access the cloudpassage API
+    baseurl = "https://api.cloudpassage.com"
     _auth = None
 
     def __post(self, endpoint, data=None, headers=None):
-        json = requests.post(self.baseurl + endpoint, data=data, headers=headers).json()
+        url = self.baseurl + endpoint
+        req = requests.post(url, data=data, headers=headers)
+        req.raise_for_status()
+        json = req.json()
         if u'error' in json:
             if json[u'error'] == u'invalid_client':
                 raise InvalidClientError(json['error_description'])
             else:
-                raise CloudPassageError(json['error'], json['error_description'])
+                raise CloudPassageError(json['error'],
+                                        json['error_description'])
         return json
 
     def login(self, keyid, secretkey):
@@ -58,23 +63,21 @@ class Token(str):
 
     def token(self):
         """Retrieve the current token, or get a new one"""
-        if datetime.now() >= self.expires:
-            endpoint = 'oauth/access_token?grant_type=client_credentials'
-            headers = dict()
-            headers['Authorization'] = 'Basic %s' % self._auth
-            json = self.__post(endpoint, None, headers)
-            self._access_token = json['access_token']
-            self.token_type = json['token_type']
-            self.expires = datetime.now() + timedelta(seconds=json['expires_in'])
-            self.scope = json['scope']
+        if datetime.now() < self.expires:
             return self._access_token
-        else:
-            return self._access_token
-
+        endpoint = '/oauth/access_token?grant_type=client_credentials'
+        headers = dict()
+        headers['Authorization'] = 'Basic %s' % self._auth
+        json = self.__post(endpoint, None, headers)
+        self._access_token = json['access_token']
+        self.token_type = json['token_type']
+        self.expires = datetime.now() + timedelta(seconds=json['expires_in'])
+        self.scope = json['scope']
+        return self._access_token
 
     def __str__(self):
         """Make this object work like a string for easy access"""
-        return self.token
+        return self.token()
 
     def __repr__(self):
         """Make this object work like a string for easy access"""
@@ -84,8 +87,8 @@ class Token(str):
 class CloudPassage:
     """A simple helper to make working with the CloudPassage API Easier"""
 
-
-    baseurl = "https://api.cloudpassage.com/" #URL to access the cloudpassage API
+    # URL to access the cloudpassage API
+    baseurl = "https://api.cloudpassage.com"
     _auth = None
 
     def __init__(self, keyid, secret_key):
@@ -98,26 +101,83 @@ class CloudPassage:
         self.token = Token()
         self.token.login(keyid, secret_key)
 
-    def __get(self, endpoint, data=None, headers=None):
-        if not headers:
-            headers = {}
-        headers['Authorization'] = 'Bearer %s' % self.token
-        json = requests.get(self.baseurl + endpoint, data=data, headers=headers).json()
+    def __get(self, endpoint):
+        headers = {'Authorization': 'Bearer %s' % self.token}
+        url = self.baseurl + endpoint
+        req = requests.get(url, headers=headers)
+        return self.__parserequest(req)
+
+    def __post(self, endpoint, data=None):
+        if data:
+            # Convert data to properly-formatted json before posting
+            data = dumps(data)
+        headers = {'Authorization': 'Bearer %s' % self.token}
+        url = self.baseurl + endpoint
+        req = requests.post(url, data=data, headers=headers)
+        return self.__parserequest(req)
+
+    def __parserequest(self, req):
+        req.raise_for_status()
+        json = req.json()
         if u'error' in json:
             if json[u'error'] == u'invalid_client':
                 raise InvalidClientError(json['error_description'])
             else:
-                raise CloudPassageError(json['error'], json['error_description'])
+                raise CloudPassageError(json['error'],
+                                        json['error_description'])
         return json
 
-    def __post(self, endpoint, data=None, headers=None):
-        if not headers:
-            headers = {}
-        headers['Authorization'] = 'Bearer %s' % self.token
-        json = requests.post(self.baseurl + endpoint, data=data, headers=headers).json()
-        if u'error' in json:
-            if json[u'error'] == u'invalid_client':
-                raise InvalidClientError(json['error_description'])
-            else:
-                raise CloudPassageError(json['error'], json['error_description'])
-        return json
+    def list_users(self):
+        """
+        Lists all available profile information, including user ID and URL to
+        the user resource, for all users in your Halo account.
+        """
+        return self.__get('/v1/users')
+
+    def get_user(self, uid):
+        """
+        Lists the profile information for a single user, specified by user ID.
+        """
+        return self.__get('/v1/users/%s' % uid)
+
+    def list_server_groups(self):
+        """
+        Returns the names and details, including group ID, of all of your
+        currently defined server groups.
+        """
+        return self.__get('/v1/groups')
+
+    def list_server_group(self, gid):
+        """
+        Returns information describing a single server group specified by group
+        ID.
+        """
+        return self.__get('/v1/groups/%s' % gid)
+
+    def create_server_group(self, name, tag, **kwargs):
+        """
+        Creates a new server group with default values that you specify, and
+        returns its information, including URL and group ID, in the response
+        body.
+        """
+        body = {"group": {}}
+        body['group']['name'] = name
+        body['group']['tag'] = tag
+        for k, v in kwargs.items():
+            body['group'][k] = v
+
+        print body
+        return self.__post('/v1/groups', body)
+
+    def update_server_group(self, gid, **kwargs):
+        """
+        Use this call to update individual attributes of the server group that
+        you specify by group ID.
+        You need to include only the attributes that you want modified; other
+        attributes of the group will remain unchanged.
+        """
+        body = {"group": {}}
+        for k, v in kwargs.items():
+            body['group'][k] = v
+
+        return self.__post('/v1/groups/%s' % gid, body)
